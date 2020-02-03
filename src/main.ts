b2dee371f80d9dffe48d46f53ece0d85caf657be
takusan23 <takumu1200@gmail.com>
2020-02-04 00:44:17 +0900

    むずい

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ export function main(param: GameMainParameterObject): void {
 	const scene = new g.Scene({
 		game: g.game,
 		// このシーンで利用するアセットのIDを列挙し、シーンに通知します
-		assetIds: ["player", "shot", "se", "karaoke", "toomo", "pentagon"]
+		assetIds: ["player", "shot", "se", "karaoke", "toomo", "inu"]
 	});
 	let time = 60; // 制限時間
 	if (param.sessionParameter.totalTimeLimit) {
@@ -78,45 +78,79 @@ export function main(param: GameMainParameterObject): void {
 		});
 		const floorBody = box.createBody(base, floorDef, floorFixDef);
 
+		interface FaceObj {
+			assetSrc: string;
+			atariHanteiList: b2.Box2DWeb.Common.Math.b2Vec2[];
+		}
+
+		// 生成するもの
+		const defultToomo: FaceObj = {
+			assetSrc: "toomo",
+			atariHanteiList: [box.vec2(7.5, -36.5),
+			box.vec2(25.5, -11.5),
+			box.vec2(18.5, 37.5),
+			box.vec2(-14.5, 36.5),
+			box.vec2(-30.5, 9.5),
+			box.vec2(-35.5, -14.5),
+			box.vec2(-15.5, -37.5)]
+		};
+		const karaoke: FaceObj = {
+			assetSrc: "inu",
+			atariHanteiList: [
+				box.vec2(12, -38.5),
+				box.vec2(27, -32.5),
+				box.vec2(36, -20.5),
+				box.vec2(45, -9.5),
+				box.vec2(49, -4.5),
+				box.vec2(49, 21.5),
+				box.vec2(37, 37.5),
+				box.vec2(-41, 32.5),
+				box.vec2(-50, 21.5),
+				box.vec2(-50, -0.5),
+				box.vec2(-41, -15.5),
+				box.vec2(-34, -25.5),
+				box.vec2(-24, -32.5),
+				box.vec2(-7, -39.5)
+			]
+		};
+		// 生成する物体の配列
+		const faceObjList: FaceObj[] = [defultToomo, karaoke]
 
 		// 画面をタッチしたとき、SEを鳴らします
 		scene.pointDownCapture.add((event) => {
+			const xPos = event.point.x;
+			const yPos = event.point.y;
+
 			// プレイヤーを生成します
+			const random = g.game.random.get(0, faceObjList.length - 1)
+			createFace(faceObjList[random], xPos, yPos);
+		});
+
+		/** 物体作成関数 */
+		const createFace = (obj: FaceObj, xPos: number, yPos: number) => {
 			const entity = new g.Sprite({
 				scene: scene,
-				src: scene.assets["toomo"],
-				width: (scene.assets["toomo"] as g.ImageAsset).width,
-				height: (scene.assets["toomo"] as g.ImageAsset).height,
-				x: event.point.x,
-				y: event.point.y
+				src: scene.assets[obj.assetSrc],
+				width: (scene.assets[obj.assetSrc] as g.ImageAsset).width,
+				height: (scene.assets[obj.assetSrc] as g.ImageAsset).height,
+				x: xPos,
+				y: yPos
 			});
 			scene.append(entity);
 			entity.modified();
-
-			const vertices = [
-				box.vec2(7.5, -36.5),
-				box.vec2(25.5, -11.5),
-				box.vec2(18.5, 37.5),
-				box.vec2(-14.5, 36.5),
-				box.vec2(-30.5, 9.5),
-				box.vec2(-35.5, -14.5),
-				box.vec2(-15.5, -37.5)
-			];
-
 			const entityFixDef = box.createFixtureDef({
 				density: 1.0, // 密度
 				friction: 0.5, // 摩擦係数
 				restitution: 0.3, // 反発係数
-				shape: box.createPolygonShape(vertices) // 形状
+				shape: box.createPolygonShape(obj.atariHanteiList) // 形状
 			});
 
 			const entityDef = box.createBodyDef({
 				type: b2.BodyType.Dynamic
 			});
-
 			box.createBody(entity, entityDef, entityFixDef);
+		};
 
-		});
 		const updateHandler = () => {
 			if (time <= 0) {
 				// RPGアツマール環境であればランキングを表示します
